44bdf8fe5108e7c7c16d8676ad28d89b9dd2b5df
rk-trl <rk.rajkumar.trl@gmail.com>
2023-10-25 15:37:35 +0530

    Single producer and single receiver
    Single producer and single receiver

---
+++ RabbitMQReceiver/RabbitMQReceiver/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+/*
+ * First example
+ */
+//var factory = new ConnectionFactory { HostName = "localhost" };
+
+//using(var connection = factory.CreateConnection())
+//{
+
+//    //channel
+//    using(var channel = connection.CreateModel())
+//    {
+//        channel.QueueDeclare("myQueue", false, false, false, null);
+
+//        //craete a consumer
+//        var consumer = new EventingBasicConsumer(channel);
+
+//        //Handle received message
+//        consumer.Received += (model, ea) =>
+//        {
+//            var body = ea.Body;
+//            var msg = System.Text.Encoding.UTF8.GetString(body.ToArray());
+//            Console.WriteLine("Message Received {0}", msg);
+//        };
+//        //start consuming
+//        channel.BasicConsume("myQueue",true,consumer);
+//        Console.WriteLine("Listening messages. press any key to stop listening");
+//        Console.ReadLine();
+
+//    }
+//}
+
+
+
+/*
+ * Second example
+ */
+var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume("demo-queue",true,consumer);
+
+Console.Read();
---
+++ RabbitMQSender/RabbitMQSender/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json.Serialization;
+
+/*
+ * First example
+ */
+
+//var factory = new ConnectionFactory { HostName = "localhost" };
+
+//using(var connection = factory.CreateConnection())
+//{
+//    using(var channel = connection.CreateModel())
+//    {
+//        string msg = string.Empty;
+//        //Queue
+//        channel.QueueDeclare("myQueue", false, false, false, null);
+//        do
+//        {
+//            var body = System.Text.Encoding.UTF8.GetBytes(msg);
+
+//            //publish message to the queue
+//            channel.BasicPublish("", "myQueue", null, body);
+//            Console.WriteLine("Message Sent");
+
+//            msg = Console.ReadLine();
+
+//        } while (!string.IsNullOrEmpty(msg));
+
+//    }
+//}
+
+/*
+ * Second example
+ */
+var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = new { Name = "Producer", Message = "Hello" };
+
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+channel.BasicPublish("","demo-queue",null,body);
